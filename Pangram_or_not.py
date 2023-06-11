ff5460e2fb242b97bd225a4f965a84493af218f6
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2023-06-11 18:31:22 +0530

    n=input() n=n.lower() k='abcdefghijklmnopqrstuvwxyz' for i in k:     if i not in n:         print(False)         break else:     print(True)

---
+++ Pangram_or_not.py
@@ -0,0 +1,15 @@
+n=input()
+n=n.lower()
+k='abcdefghijklmnopqrstuvwxyz'
+for i in k:
+    if i not in n:
+        print(False)
+        break
+else:
+    print(True)
+   
+        
+        
+        
+        
+  
